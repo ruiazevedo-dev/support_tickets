0a6667d84c1cc7c43f27ccdc8a20beaba87f76f8
Rui Azevedo <ruiazevedodev@outlook.com>
2023-08-10 14:38:59 +0100

    mutator on User

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
